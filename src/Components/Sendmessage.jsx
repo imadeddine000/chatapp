251d8f59f9a2731c924f910de5b705d0c1dd322e
imadeddinekebour <imadeddinekebourlite@gmail.com>
2022-07-17 18:44:55 +0100

    fix message component

--- src/Components/Sendmessage.jsx
+++ src/Components/Sendmessage.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import {AiFillPlusCircle} from 'react-icons/ai'
-import {TbPaperclip} from 'react-icons/tb'
-import {BsEmojiSmileFill} from 'react-icons/bs'
 import {HiThumbUp} from 'react-icons/hi'
+import {BiSend} from 'react-icons/bi'
+
 const Sendmessage = () => {
   return (
     <div className='flex p-2 w-[100%]'>
         <div className='flex items-center space-x-0 grow'>
-            <div className='text-2xl text-violet-900 p-0 hover:bg-gray-300 rounded-full cursor-pointer'>
-                <AiFillPlusCircle/>
-            </div>
             <div className='flex items-center grow bg-gray-300 rounded-2xl p-1 '>
                 <input className='grow outline-none bg-gray-300 pl-1' type="text" name="" id=""  placeholder='Type a message ...'/>
                 <div className='flex items-center text-xl space-x-1 '>
-                    <div className='text-violet-600 text-xl hover:rounded-full hover:bg-gray-400 p-1 cursor-pointer'>
-                    <BsEmojiSmileFill/>
-                    </div>
-                    <div className='text-violet-600 text-xl  hover:rounded-full hover:bg-gray-400 p-1 cursor-pointer'>
-                    <TbPaperclip/>
+                    <div className='text-violet-600 text-2xl hover:rounded-full hover:bg-gray-400 p-1 cursor-pointer'>
+                    <BiSend/>
                     </div>
                 </div>
                 
             </div>
-                <div className='text-violet-600 text-2xl cursor-pointer '>
+                <div className='text-violet-600 text-2xl cursor-pointer flex items-center '>
+                    <button>
                     <HiThumbUp/>
+                    </button>
                 </div>
         </div>
     </div>
